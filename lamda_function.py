5193bdd02f171235ddd702daa7936c39e53ae37f
r105046 <alfonso.fezza@cnhind.com>
2024-06-15 15:06:18 +0200

    Add test mode to work with multiple db

--- lamda_function.py
+++ lamda_function.py
@@ -15,7 +15,14 @@ def lambda_handler(event, context):
         rcvd_message = telegramAPI.BotMessage(request=event["body"]) # Take the json string of the content from the input event 
         
         if(telegramAPI.BOT_NAME in rcvd_message.text):    # Ignoro i messaggi che non sono destinati al bot
+            if("test:" in rcvd_message.text):
+                chat_to_send = rcvd_message.chat_id
+                rcvd_message.chat_id = rcvd_message.text[rcvd_message.text.find("test:")+5:].split()[0]
+            
             bot_reply = fantaOP.run_command(rcvd_message)
+            
+            if("test:" in rcvd_message.text):
+                bot_reply.chat_id = chat_to_send
             bot_reply.sendMessage() # Invio la risposta alla chat               
     except Exception as e: 
         logger.setLevel("ERROR")
